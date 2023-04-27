79d256976eafb21afbb35b0c29afbc0832dca5c2
Jason Koolman <jason.koolman@hotmail.com>
2023-04-28 00:16:45 +0200

    feat: use SWC

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -5,6 +5,7 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
 /** @type {import('@nrwl/next/plugins/with-nx').WithNxOptions} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
